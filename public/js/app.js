876c1787e37b9fd8d705125a3f9161d3a1551d71
Jack Pilowsky <jackpilowsky@yahoo.com>
2021-09-19 11:14:17 -0700

    renderTagList and tagListClicked methods

--- public/js/app.js
+++ public/js/app.js
@@ -21,6 +21,7 @@ export default class TagBrowserWidget {
   getElements() {
     this.tagList = this.config.element.querySelectorAll('.tag-list')[0];
     // find and store other elements you need
+    this.matchingItemsList = this.config.element.querySelectorAll('.matching-items-list')[0]
   }
 
   bindEventListeners() {
@@ -30,11 +31,33 @@ export default class TagBrowserWidget {
 
   renderTagList() {
     // render the list of tags from this.data into this.tagList
+    const fullTagList =  this.data.flatMap(book=> book.tags).sort()
+    // dedup
+    const uniqueTags = [...new Set(fullTagList)];
+    //render 
+    const html = uniqueTags.map(tag=>`<li><span class="tag is-link">${tag}</span></li>`).join('')
+    this.tagList.innerHTML = html
   }
 
   tagListClicked(event) {
     console.log('tag list (or child) clicked', event);
     // check to see if it was a tag that was clicked and render
     // the list of series that have the matching tags
+    const tag = event.target.innerHTML
+    const matchingBooks = this.data.filter(book=>{
+      return book.tags.find(bookTag =>bookTag.toLowerCase() === tag.toLowerCase())
+    })
+    // update the active class
+    const active = this.tagList.querySelectorAll('.active')
+    if(active.length > 0){
+      active.forEach(li => li.classList.remove('active'))
+    }
+    event.target.classList.add('active')
+    // render list
+    const html = matchingBooks.map(book=>`<li>${book.title}</li>`).join('')
+    this.matchingItemsList.innerHTML = html
+    // render list title
+    const subtitle = this.matchingItemsList.previousElementSibling;
+    subtitle.innerHTML = tag
   }
 }
